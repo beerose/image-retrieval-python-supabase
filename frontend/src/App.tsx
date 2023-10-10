00f5e520fdab2f500854cb21bb0c81c4546485d1
beerose <alexsandra.sikora@gmail.com>
2023-10-10 12:07:30 +0200

    backend integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,48 @@
+import { ChangeEvent, useState } from 'react'
+
+type Image = {
+  name: string
+  url: string
+  score: number
+}
+
 export default function App() {
+  const [images, setImages] = useState<Image[]>([])
+  const [file, setFile] = useState<File | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<Error | null>(null)
+
+  const onFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+
+    const selectedFile = e.target.files && e.target.files[0]
+    if (selectedFile) {
+      setFile(selectedFile)
+      setLoading(true)
+      setError(null)
+      try {
+        const formData = new FormData()
+        formData.append('file', selectedFile)
+
+        const response = await fetch('http://localhost:8000/search/', {
+          method: 'POST',
+          body: formData,
+          headers: {
+            accept: 'application/json',
+          },
+        })
+        const files = await response.json()
+        if (Array.isArray(files)) {
+          setImages(files.filter((file) => file.score < 0.5))
+        }
+        setLoading(false)
+      } catch (err) {
+        setError(err as Error)
+        setLoading(false)
+      }
+    }
+  }
+
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-3xl p-10">
@@ -17,9 +61,9 @@ export default function App() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                 />
               </svg>
@@ -35,28 +79,68 @@ export default function App() {
               id="dropzone-file"
               type="file"
               className="hidden"
+              onChange={onFileChange}
             />
           </label>
         </div>
+        {file && (
+          <div className="mt-6">
+            <h1 className="py-4 bg-white pr-3 text-lg font-semibold leading-6 text-slate-600">
+              Your image
+            </h1>
+            <img
+              src={file ? URL.createObjectURL(file) : ''}
+              alt=""
+              className="object-contain w-44"
+            />
+          </div>
+        )}
         <div className="mt-6">
           <h1 className="py-4 bg-white pr-3 text-lg font-semibold leading-6 text-slate-600">
             Results
           </h1>
+          {loading && (
+            <div className="py-2.5 text-sm font-medium text-gray-900 bg-white rounded-lg inline-flex items-center">
+              <svg
+                aria-hidden="true"
+                role="status"
+                className="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="#1C64F2"
+                />
+              </svg>
+              Loading...
+            </div>
+          )}
+          {error && (
+            <p className="text-xs text-red-500">
+              Something went wrong. Please try again.
+            </p>
+          )}
           <ul
             role="list"
             className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 xl:gap-x-8"
           >
-            {files.map((file) => (
+            {images.map((file) => (
               <li
-                key={file.source}
+                key={file.name}
                 className="relative space-y-1"
               >
                 <p className="pointer-events-none block text-md font-medium text-gray-600">
-                  Similarity: {file.similarity}
+                  Similarity: {(1 - file.score).toFixed(2)}
                 </p>
                 <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
                   <img
-                    src={file.source}
+                    src={file.url}
                     alt=""
                     className="pointer-events-none object-cover group-hover:opacity-75"
                   />
@@ -75,11 +159,3 @@ export default function App() {
     </div>
   )
 }
-
-const files = [
-  {
-    similarity: '87%',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-]
